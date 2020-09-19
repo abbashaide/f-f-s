3e8c6ea59dc54498f51eaad6ca8ad2b667751778
abbashaide <57639185+abbashaide@users.noreply.github.com>
2020-09-19 15:22:59 +0500

    edit Navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,24 @@
 import	React from 'react';
 import './Navigation.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUserCircle, faPlus, faFingerprint } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUserCircle, faPlus, faFingerprint, faFileUpload, faLink } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
 	return(
-		<nav className = 'bb b--black-20 bg-white tc mw10 center' style ={{display: 'flex', justifyContent: 'flex-end', flexWrap: 'wrap'}}>
-			<p className ='f4 fw6 ttc tracked link dim dib black pa2 pointer padd mt1 mb0'><FontAwesomeIcon icon={faHome} /> Home</p>
-			<p className ='f4 fw6 ttc tracked link dim black pa2 pointer padd mt1 mb1'><FontAwesomeIcon icon={faUserCircle} /> Profile</p>
-			<p className ='f4 fw6 ttc tracked link dim black pa2 pointer padd mt1 mb1'><FontAwesomeIcon icon={faPlus} /> Upload</p>
-			<p className ='f4 fw6 ttc tracked link dim black pa2 pointer padd mt1 mb1'><FontAwesomeIcon icon={faFingerprint} /> Identify</p>
-		</nav>
+		<div>
+			<nav className = 'bb b--black-20 bg-white tc mw10 center' style ={{display: 'flex', justifyContent: 'flex-end', flexWrap: 'wrap'}}>
+				<p className ='f4 fw6 ttc tracked link dim dib black pa2 pointer padd mt1 mb0'><FontAwesomeIcon icon={faHome} /> Home</p>
+				<p className ='f4 fw6 ttc tracked link dim black pa2 pointer padd mt1 mb1'><FontAwesomeIcon icon={faUserCircle} /> Profile</p>
+				<p className ='f4 fw6 ttc tracked link dim black pa2 pointer padd mt1 mb1'><FontAwesomeIcon icon={faPlus} /> Upload</p>
+				<p className ='f4 fw6 ttc tracked link dim black pa2 pointer padd mt1 mb1'><FontAwesomeIcon icon={faFingerprint} /> Identify</p>
+			</nav>
+
+			<nav className = 'bb b--black-20 bg-white tc mw10 center' style ={{display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap'}}>
+				<p className ='f5 fw6 ttc tracked link dim dib black pa2 pointer paddd mt0 mb0'><FontAwesomeIcon icon={faFileUpload} /> File</p>
+				<p className ='f5 fw6 ttc tracked link dim dib black pa2 pointer paddd mt0 mb0'><FontAwesomeIcon icon={faLink} /> Url</p>
+			</nav>
+
+		</div>
 	);
 }
 
