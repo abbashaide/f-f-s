c1f1b23a66115d806d26060da8b7d8a467677042
abbashaide <57639185+abbashaide@users.noreply.github.com>
2020-09-21 23:57:14 +0500

    edit UserInput

--- src/components/MessageBox/MessageBox.js
+++ src/components/MessageBox/MessageBox.js
@@ -3,7 +3,7 @@ import './MessageBox.css'
 
 const MessageBox = () => {
 	return(
-		<div className='display'>
+		<div className='display mt1 mb1'>
 			<div className='alert alert-success'>
 				<p className='alert-heading'>Well Done!</p>
 				<p>This is Gandalf and Robin</p>
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './UserInput.css';
 
 const UserInput = () => {
 	return(
-		<div></div>
+		<div className='body'>
+				<p className=''>Whose thy Friend or Enemy!</p>
+
+			<div className='radios'>
+				<label>
+					<input className='' type="radio" id="fr" name="rel" value="fr" checked={true} /> Friend
+				</label>
+
+				<label>
+				<input className='' type="radio" id="en" name="rel" value="en" /> Enemy
+				</label>
+			</div>
+			<label>
+				Enter name: <input className='' type="text" id="name" name="name" placeholder='type...' />
+			</label>
+
+		</div>
 	);
 }
 
